ee383dd85d7da0c2070434d70c44db721212bad0
Ufuk Caglayan <umufucuk@gmail.com>
2026-02-06 20:16:36 +0430

    m

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `practices_db`;
+USE `practices_db`;
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(191) NOT NULL,
+  `email` VARCHAR(191) NOT NULL,
+  `password` VARCHAR(191) NOT NULL,
+  `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `confirmedEmail` BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `User_email_key` (`email`)
+);
